3691a477d6b414da1bbdc246447631745e4c6ced
Ziqul <m.s.v.00a@gmail.com>
2019-03-29 09:15:09 +0200

    Added support for frame extractor

--- src/app/HTTP/router/index.ts
+++ src/app/HTTP/router/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import * as bodyParser from 'body-parser';
 
 import Home from 'HTTP/controllers/Home';
 
@@ -6,6 +7,16 @@ import apiRouter from 'HTTP/router/api';
 import staticRouter from 'HTTP/router/static';
 import storageRouter from 'HTTP/router/storage';
 import { Request, Response } from 'express';
+import { Blur } from 'lib/render/effects/Blur';
+import { StreamFramesExtractor } from 'lib/render/StreamFramesExtractor';
+import { TheMachine } from 'lib/render';
+import { MongoVideos } from 'storage/Video/MongoVideos';
+import { Three } from 'lib/render/API/3';
+import { RGBA32toJPEG } from 'lib/streams';
+import { EffectsRepo } from 'lib/render/EffectsRepo';
+import { Effect } from 'lib/render/effects/Effect';
+import { EffectSetting } from 'lib/render/Types';
+import { RGBA32Frame } from 'lib/ffmpeg';
 
 
 let router: Router = Router();
@@ -19,14 +30,38 @@ router.get('/', Home.index);
 router.get('/', Home.dashboard);
 
 // Kill me now
-function frameCtrl(req: Request, resp: Response) {
+async function frameCtrl(req: Request, resp: Response) {
   let project = req.body.project;
   let millis = req.body.millis;
 
+  let machine = new TheMachine(
+    project,
+    new StreamFramesExtractor(project, new MongoVideos),
+    new Three(project),
+    new TempEffectsRepo()
+  );
 
+  machine.stream.pipe(new RGBA32toJPEG(project.settings.title));
+  let result = await machine.renderFrame(millis / 40, millis / 40);
+
+  var path = require('path');
+  resp.sendFile(path.resolve(`storage/out/${project.settings.title}/1.jpg`));
 }
 
-router.get('/frame', frameCtrl);
+class TempEffectsRepo implements EffectsRepo<RGBA32Frame> {
+
+  getEffectById(id: string): Effect<RGBA32Frame> {
+    if (id === 'rotating_box') {
+      return new Blur();
+    } else {
+      throw "No effect with such id";
+    }
+  }
+}
+
+router.use(bodyParser());
+
+router.post('/frame', frameCtrl);
 
 
 export default router;
