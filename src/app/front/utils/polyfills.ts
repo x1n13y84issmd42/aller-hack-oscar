a4dbb8765c3e79baa8fe68f65207595f98599659
Eugene Merkulov <gaddeena@gmail.com>
2019-09-22 17:28:55 +0300

    Removed webpack & react

--- src/app/front/utils/polyfills.ts
+++
@@ -1,4 +0,0 @@
-import 'core-js/fn/object';
-import 'core-js/fn/promise';
-import 'core-js/fn/array';
-import 'babel-polyfill';
--- src/app/lib/render/FrameRenderMachine.ts
+++ src/app/lib/render/FrameRenderMachine.ts
@@ -7,106 +7,106 @@ import { FrameBlender } from 'lib/render/FrameBlender';
 
 export class FrameRenderMachine<V extends Types.VideoDesc, F> {
 
-  private gl: WebGLRenderingContext;
+	private gl: WebGLRenderingContext;
 
-  constructor(
-    private project: Types.Project,
-    private videosRepo: IVideos<V>,
-    private effectsRepo: EffectsRepo<F>,
-    private frameExtractor: FrameExtractor<V, F>,
-    private frameBlender: FrameBlender<F>,
-    private webgl?: WebGLRenderingContext
-  ) {
-    this.gl = webgl || gl(project.settings.width, project.settings.height, {
-      preserveDrawingBuffer: true
-    });
-  }
+	constructor(
+		private project: Types.Project,
+		private videosRepo: IVideos<V>,
+		private effectsRepo: EffectsRepo<F>,
+		private frameExtractor: FrameExtractor<V, F>,
+		private frameBlender: FrameBlender<F>,
+		private webgl?: WebGLRenderingContext
+	) {
+		this.gl = webgl || gl(project.settings.width, project.settings.height, {
+			preserveDrawingBuffer: true
+		});
+	}
 
-  async render(timestamp: number): Promise<F> {
-    let targetVideoIdsWithOffsetsAndEffects =
-      await this.getVideoIdWithOffsetAndEffects(this.project.timelines, timestamp);
+	async render(timestamp: number): Promise<F> {
+		let targetVideoIdsWithOffsetsAndEffects =
+			await this.getVideoIdWithOffsetAndEffects(this.project.timelines, timestamp);
 
-    let targetFramesWithEffects =
-      await this.getFramesWithEffects(targetVideoIdsWithOffsetsAndEffects);
+		let targetFramesWithEffects =
+			await this.getFramesWithEffects(targetVideoIdsWithOffsetsAndEffects);
 
-    let processedFrames: F[];
+		let processedFrames: F[];
 
-    for (let fae of targetFramesWithEffects) {
-      let workingFrame = fae.frame;
+		for (let fae of targetFramesWithEffects) {
+			let workingFrame = fae.frame;
 
-      for (let effect of fae.effects) {
-        workingFrame =
-          this.effectsRepo.getEffectById(effect.id).processFrame(
-            workingFrame, effect.settings);
-      }
+			for (let effect of fae.effects) {
+				workingFrame =
+					this.effectsRepo.getEffectById(effect.id).processFrame(
+						workingFrame, effect.settings);
+			}
 
-      processedFrames.push(workingFrame);
-    }
+			processedFrames.push(workingFrame);
+		}
 
-    let finalFrame = this.frameBlender.getDefaultFrame();
+		let finalFrame = this.frameBlender.getDefaultFrame();
 
-    for (let frame of processedFrames) {
-      finalFrame = this.frameBlender.blendFrames(finalFrame, frame);
-    }
+		for (let frame of processedFrames) {
+			finalFrame = this.frameBlender.blendFrames(finalFrame, frame);
+		}
 
-    return finalFrame;
-  }
+		return finalFrame;
+	}
 
-  getVideoIdWithOffsetAndEffects(
-    timelines: Types.Timeline[],
-    timestamp: number
-  ):
-    VideoWithOffsetAndEffects[]
-  {
-    let result: VideoWithOffsetAndEffects[];
+	getVideoIdWithOffsetAndEffects(
+		timelines: Types.Timeline[],
+		timestamp: number
+	):
+		VideoWithOffsetAndEffects[]
+	{
+		let result: VideoWithOffsetAndEffects[];
 
-    for (let timeline of timelines) {
-      for (let clip of timeline.entities) {
-        if (clip.timelinePosition.start <= timestamp &&
-            clip.timelinePosition.end >= timestamp
-        ) {
-          let fromTimelineStartOffset =
-            timestamp - clip.timelinePosition.start;
+		for (let timeline of timelines) {
+			for (let clip of timeline.entities) {
+				if (clip.timelinePosition.start <= timestamp &&
+						clip.timelinePosition.end >= timestamp
+				) {
+					let fromTimelineStartOffset =
+						timestamp - clip.timelinePosition.start;
 
-          result.push({
-            videoId: clip.videoId,
-            offset: clip.clipping.start + fromTimelineStartOffset,
-            effects: clip.effects
-          } as VideoWithOffsetAndEffects);
-        }
-      }
-    }
+					result.push({
+						videoId: clip.videoId,
+						offset: clip.clipping.start + fromTimelineStartOffset,
+						effects: clip.effects
+					} as VideoWithOffsetAndEffects);
+				}
+			}
+		}
 
-    return result;
-  }
+		return result;
+	}
 
-  async getFramesWithEffects(
-    vofs: VideoWithOffsetAndEffects[]
-  ):
-    Promise<FrameWithEffects<F>[]>
-  {
-    let result: FrameWithEffects<F>[];
+	async getFramesWithEffects(
+		vofs: VideoWithOffsetAndEffects[]
+	):
+		Promise<FrameWithEffects<F>[]>
+	{
+		let result: FrameWithEffects<F>[];
 
-    for (let vof of vofs) {
-      result.push({
-        frame: this.frameExtractor.extractFrame(
-          await this.videosRepo.get(vof.videoId),
-          vof.offset),
-        effects: vof.effects
-      } as FrameWithEffects<F>);
-    }
+		for (let vof of vofs) {
+			result.push({
+				frame: this.frameExtractor.extractFrame(
+					await this.videosRepo.get(vof.videoId),
+					vof.offset),
+				effects: vof.effects
+			} as FrameWithEffects<F>);
+		}
 
-    return result;
-  }
+		return result;
+	}
 }
 
 type VideoWithOffsetAndEffects = {
-  videoId: string,
-  offset: number,
-  effects: Types.EffectDesc[]
+	videoId: string,
+	offset: number,
+	effects: Types.EffectDesc[]
 }
 
 type FrameWithEffects<F> = {
-  frame: F,
-  effects: Types.EffectDesc[]
+	frame: F,
+	effects: Types.EffectDesc[]
 }
